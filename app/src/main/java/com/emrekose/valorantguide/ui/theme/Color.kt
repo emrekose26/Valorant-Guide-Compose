2fcad100ec28736dd49efa02a1745fed48932a72
Emre Köse <cs.emrekose@gmail.com>
2022-07-19 15:38:33 +0300

    Colors and font added

--- app/src/main/java/com/emrekose/valorantguide/ui/theme/Color.kt
+++ app/src/main/java/com/emrekose/valorantguide/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.emrekose.valorantguide.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Mandy = Color(0xFFDD4854)
+val RedOrange = Color(0xFFFF2B3C)
+val WoodyBrown = Color(0xFF4A333A)
+val Malibu = Color(0xFF71ACFC)
--- app/src/main/java/com/emrekose/valorantguide/ui/theme/Theme.kt
+++ app/src/main/java/com/emrekose/valorantguide/ui/theme/Theme.kt
@@ -7,15 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Mandy,
+    primaryVariant = WoodyBrown,
+    secondary = Malibu,
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = RedOrange,
+    primaryVariant = WoodyBrown,
+    secondary = Malibu
 
     /* Other default colors to override
     background = Color.White,
